44281ed0e075449774d87e15c4cdd4f1d4dedaca
yite <coastq22889@icloud.com>
2024-02-12 16:21:26 +0800

    remove unnecessary func

--- utils/trans_rstdepth.go
+++ utils/trans_rstdepth.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	pb "github.com/BullionBear/binance-mongo/generated/proto/rstdepth"
-	"github.com/BullionBear/binance-mongo/model"
 	"github.com/adshao/go-binance/v2"
 )
 
@@ -18,27 +17,6 @@ func BinanceDepthToGrpcEvent(response *binance.DepthResponse, symbol string) *pb
 	return &pb.DepthResponse{
 		LastUpdateID: response.LastUpdateID,
 		Symbol:       symbol,
-		Bids:         bids,
-		Asks:         asks,
-	}
-}
-
-func GrpcDepthToMongoEvent(event *pb.DepthResponse) *model.DepthResponse {
-	bids := make([]model.Bid, len(event.Bids))
-	for i, bid := range event.Bids {
-		bids[i] = model.Bid{Price: bid.Price, Quantity: bid.Quantity}
-	}
-
-	asks := make([]model.Ask, len(event.Asks))
-	for i, ask := range event.Asks {
-		// Similar handling for asks as for bids.
-		asks[i] = model.Ask{Price: ask.Price, Quantity: ask.Quantity}
-	}
-
-	// Return a pointer to the constructed WsDepthEvent, filled with the converted slices.
-	return &model.DepthResponse{
-		LastUpdateID: event.LastUpdateID,
-		Symbol:       event.Symbol,
 		Bids:         bids,
 		Asks:         asks,
 	}
--- utils/trans_wsdepth.go
+++ utils/trans_wsdepth.go
@@ -1,10 +1,7 @@
 package utils
 
 import (
-	"time"
-
 	pb "github.com/BullionBear/binance-mongo/generated/proto/wsdepth"
-	"github.com/BullionBear/binance-mongo/model"
 	"github.com/adshao/go-binance/v2"
 )
 
@@ -20,30 +17,6 @@ func BinanceWsDepthToGrpcEvent(event *binance.WsDepthEvent) *pb.WsDepthEvent {
 	return &pb.WsDepthEvent{
 		Event:         event.Event,
 		Time:          event.Time,
-		Symbol:        event.Symbol,
-		LastUpdateID:  event.LastUpdateID,
-		FirstUpdateID: event.FirstUpdateID,
-		Bids:          bids,
-		Asks:          asks,
-	}
-}
-
-func GrpcWsDepthToMongoEvent(event *pb.WsDepthEvent) *model.WsDepthEvent {
-	bids := make([]model.Bid, len(event.Bids))
-	for i, bid := range event.Bids {
-		bids[i] = model.Bid{Price: bid.Price, Quantity: bid.Quantity}
-	}
-
-	asks := make([]model.Ask, len(event.Asks))
-	for i, ask := range event.Asks {
-		// Similar handling for asks as for bids.
-		asks[i] = model.Ask{Price: ask.Price, Quantity: ask.Quantity}
-	}
-
-	// Return a pointer to the constructed WsDepthEvent, filled with the converted slices.
-	return &model.WsDepthEvent{
-		Event:         event.Event,
-		Time:          time.Unix(0, event.Time*int64(time.Millisecond)),
 		Symbol:        event.Symbol,
 		LastUpdateID:  event.LastUpdateID,
 		FirstUpdateID: event.FirstUpdateID,
